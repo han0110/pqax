d316d95ff0b70a0a8dec30d67c9f9d7e13f6a59b
han0110 <b05901044@ntu.edu.tw>
2021-06-20 15:30:20 +0000

    chore: copy pqclean ntrulpr761

---
+++ crypto_kem/ntrulpr761/aarch64/api.h
@@ -0,0 +1,15 @@
+#ifndef API_H
+#define API_H
+
+#include <stdint.h>
+
+#define CRYPTO_SECRETKEYBYTES 1294
+#define CRYPTO_PUBLICKEYBYTES 1039
+#define CRYPTO_CIPHERTEXTBYTES 1167
+#define CRYPTO_BYTES 32
+
+int crypto_kem_keypair(uint8_t *pk, uint8_t *sk);
+int crypto_kem_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
+int crypto_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
+
+#endif
